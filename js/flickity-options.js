c1d091342de7350a00e98133652f3bdc2ac11f76
Becky <deadskerry@gmail.com>
2015-10-28 17:14:34 +0100

    about-us done

--- js/flickity-options.js
+++ js/flickity-options.js
@@ -1,7 +1,6 @@
 var elem = document.getElementsByClassName('.carousel');
 var flkty = new Flickity( elem, {
   // options
-  cellAlign: 'left',
   contain: true,
   wrapAround: true,
   freeScroll: true
